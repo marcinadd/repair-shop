18a4de3c4d32e56698439336d1ce4c66e662f217
marcinadd <marcinadd@gmail.com>
2020-04-19 10:12:16 +0200

    Initial commit

---
+++ src/main/java/com/marcinadd/repairshop/RepairShopApplication.java
@@ -0,0 +1,13 @@
+package com.marcinadd.repairshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepairShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcinadd/repairshop/RepairShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcinadd.repairshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
